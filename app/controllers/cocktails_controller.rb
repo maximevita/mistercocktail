a9bf5f91ed3e833fdd594a0debf1fdf6b868f8ee
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-11-28 12:01:30 +0100

    added possibility to add pictures with a cocktail and display it on cocktail page

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -27,6 +27,6 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
   end
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :picture)
   end
 end
